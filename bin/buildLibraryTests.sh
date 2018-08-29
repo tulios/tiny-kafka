c925edb922f8b830bb901e395c31ec80c4bb6ef5
tulios <ornelas.tulio@gmail.com>
2018-08-29 21:35:28 +0200

    Update build/run scripts

--- bin/buildLibraryTests.sh
+++ bin/buildLibraryTests.sh
@@ -2,15 +2,22 @@
 
 LIBRARY="$1"
 
+green() {
+  echo -e "\033[0;32m$1\033[0m"
+}
+
 build_kafkajs() {
+  green "\nBuilding: kafkajs"
   docker build -t tiny-kafka-lib-kafkajs -f libraryTests/kafkajs/Dockerfile .
 }
 
 build_ruby_kafka() {
+  green "\nBuilding: ruby-kafka"
   docker build -t tiny-kafka-lib-ruby-kafka -f libraryTests/ruby-kafka/Dockerfile .
 }
 
 build_java_0_11() {
+  green "\nBuilding: java 0.11"
   docker build -t tiny-kafka-lib-java-0-11 -f libraryTests/java_0_11/Dockerfile .
 }
 
@@ -28,4 +35,5 @@ case ${LIBRARY} in
     echo -e "Running for all libraries (kafkajs, ruby_kafka, java_0_11)"
     build_kafkajs
     build_ruby_kafka
+    build_java_0_11
 esac
--- bin/runLibraryTests.sh
+++ bin/runLibraryTests.sh
@@ -2,15 +2,22 @@
 
 LIBRARY="$1"
 
+green() {
+  echo -e "\033[0;32m$1\033[0m"
+}
+
 run_kafkajs() {
+  green "\nRunning: kafkajs"
   docker run -it tiny-kafka-lib-kafkajs
 }
 
 run_ruby_kafka() {
+  green "\nRunning: ruby-kafka"
   docker run -it tiny-kafka-lib-ruby-kafka
 }
 
 run_java_0_11() {
+  green "\nRunning: java 0.11"
   docker run -it tiny-kafka-lib-java-0-11
 }
 
@@ -28,4 +35,5 @@ case ${LIBRARY} in
     echo -e "Running for all libraries (kafkajs, ruby_kafka, java_0_11)"
     run_kafkajs
     run_ruby_kafka
+    run_java_0_11
 esac
