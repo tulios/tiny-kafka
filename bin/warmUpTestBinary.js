e84d71915632776dd3b0ceb3e7e20f94ca56cfe9
tulios <ornelas.tulio@gmail.com>
2018-09-18 23:34:36 +0200

    Fix warmup script

--- bin/warmUpTestBinary.js
+++ bin/warmUpTestBinary.js
@@ -5,7 +5,7 @@ const util = require('util')
 const childProcess = require('child_process')
 const exec = util.promisify(childProcess.exec)
 
-const BINARY_NAME = process.platform === 'darwin' ? 'tiny-kafka-macos' : 'tiny-kafka-linux'
+const BINARY_NAME = process.platform === 'darwin' ? 'tiny-kafka-macos' : 'tiny-kafka'
 const BINARY = path.join(__dirname, '../dist', BINARY_NAME)
 
 exec(BINARY).catch(console.error)
